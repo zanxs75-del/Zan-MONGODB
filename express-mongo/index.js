ab2fa261999de031ae60d8a873664df0f994e6d1
zanxs75-del <zanxs.75@gmail.com>
2026-06-25 08:28:58 +0000

    TRY

--- express-mongo/index.js
+++ express-mongo/index.js
@@ -2,30 +2,36 @@ const express = require('express');
 require('dotenv').config();
 const {connect} = require('./db');
 const mongoUri = process.env.MONGO_URI;
+const cors = require("cors");
 const dbName = "sample_mflix;"
+
+
+
 
 // 1. create the express application
 const app = express();
 // 2. use the JSON middleware so that we can recieve JSON requests
 app.use(express.json());
-// 3. route
-app.get('/health', function (req, res) {
-    res.json({
-        "message": "I'm alive!"
+
+
+
+async function main() {
+  try {
+    const db = await connect(mongoUri, dbname);
+    console.log('Connected to MongoDB');
+    // Add your route code here
+	  app.get("/", function(req, res) {
+     
     })
-})
-async function main(){
-
-
-  
-}       
+  } catch (error) {
+    console.error('Error connecting to MongoDB', error);
+  }
+}
 main();
+   
 
-
-
-
-
-
+    
+ 
 
 app.listen(3000, function () {
     console.log("Server has started");
