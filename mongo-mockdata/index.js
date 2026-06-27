2ed3f02d39d43f04c8777f953894c3739286edd6
zanxs75-del <zanxs.75@gmail.com>
2026-06-27 20:30:59 +0000

    HALF

--- mongo-mockdata/index.js
+++ mongo-mockdata/index.js
@@ -9,18 +9,39 @@ const app = express();
 // 2. use the JSON middleware so that we can recieve JSON requests
 app.use(express.json());
 
-async function main()
-{
+async function main() {
     const db = await connect(process.env.MONGO_URI, "mongodb_data");
 
     // routes will be here
-    app.get("/api/tabulate", async function(req, res){
+    app.get("/api/tabulate", async function(req, res) {
         const criteria = {};
 
         if (req.query.name) {
             criteria.name = {
                 $regex: req.query.name,
                 $options: "i" 
+            };
+        }
+
+        if (req.query.tags) {
+            const wantedTags = req.query.tags.split(",");
+            criteria['tags.name'] = {
+                "$in": wantedTags
+            };
+        }
+
+        // Expecting req.query.conditions to be a comma-delimited string
+        if (req.query.conditions) {
+            const conditionsArray = req.query.conditions.split(",");
+            const regexArray = [];
+
+            for (let conditions of conditionsArray) {
+                
+                regexArray.push(new RegExp(conditions.trim(), "i")); 
+            }
+
+            criteria["conditions.name"] = {
+                $in: regexArray
             };
         }
 
